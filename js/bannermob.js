05427fabb3843199b1797046a33ac698bb5040d0
PieroSanchez <pierosancas@gmail.com>
2025-05-19 19:58:36 -0500

    portadas en ingles

--- js/bannermob.js
+++ js/bannermob.js
@@ -1,22 +1,29 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const images = document.querySelectorAll(".images img");
+  const images = document.querySelectorAll(".images img");
 
-    function updateImages() {
-        const isMobile = window.innerWidth <= 540;
-    
-        images.forEach((image) => {
-          if (isMobile) {
-            image.src = `./assets/04.02.25(BANNNER)GP2.png`;
-          } else {
-            image.src = `./assets/bannergp.png`;
-          }
-        });
+  function updateImages() {
+    const isMobile = window.innerWidth <= 540;
+
+    // Detectar si la URL tiene "en" (por ejemplo en.html, /en/index.html, etc)
+    const currentPath = window.location.pathname.toLowerCase();
+    const isEnglish = currentPath.includes("/en") || currentPath.includes("en.html");
+
+    images.forEach((image) => {
+      if (isMobile) {
+        image.src = isEnglish
+          ? "./assets/04.02.25(BANNNER)GP2i.png"
+          : "./assets/04.02.25(BANNNER)GP2.png";
+      } else {
+        image.src = isEnglish
+          ? "./assets/04.02.25(BANNNER)GPI.png"
+          : "./assets/bannergp.png";
       }
-    
-      // Llama a la función al cargar la página y al redimensionar
-      updateImages();
-      window.addEventListener("resize", updateImages);
+    });
+  }
 
-    images[0].classList.add("active"); 
-    setInterval(changeSlide, 3500); 
+  updateImages();
+  window.addEventListener("resize", updateImages);
+
+  images[0].classList.add("active");
+  setInterval(changeSlide, 3500);
 });
